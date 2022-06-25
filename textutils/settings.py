177d9982dbeb83009769f4fe3cd3056e7b89eee1
varaliyamohammed8097 <varaliyamohammed1@gmail.com>
2022-06-25 12:52:38 +0530

    deploying on heroku

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_t)mplh&350&v&21hc52s*!3035lz$pwb^tg1a-7p+*_k=glo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','textutilssystem.herokuapp.com']
 
 
 # Application definition
