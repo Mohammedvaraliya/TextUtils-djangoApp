c7d10e8bb73d7ba3b041506eadf5cc5449de8276
varaliyamohammed8097 <varaliyamohammed1@gmail.com>
2022-06-26 15:32:34 +0530

    fixed bug

--- textutils/views.py
+++ textutils/views.py
@@ -11,72 +11,79 @@ def index(request):
 def analyze(request):
     #Get the text
     djtext = request.POST.get('text', 'default')
+    if djtext < '0':
+        return render(request, 'analyze_if_not_written.html')
 
-    # Check checkbox values
-    removepunc = request.POST.get('removepunc', 'off')
-    fullcaps = request.POST.get('fullcaps', 'off')
-    newlineremover = request.POST.get('newlineremover', 'off')
-    extraspaceremover = request.POST.get('extraspaceremover', 'off')
-    numberremover = request.POST.get('numberremover','off')
+    else:
+        #Get the text
+        djtext = request.POST.get('text', 'default')
 
-    #Check which checkbox is on
-    if removepunc == "on":
-        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-        analyzed = ""
-        for char in djtext:
-            if char not in punctuations:
-                analyzed = analyzed + char
+        # Check checkbox values
+        removepunc = request.POST.get('removepunc', 'off')
+        fullcaps = request.POST.get('fullcaps', 'off')
+        newlineremover = request.POST.get('newlineremover', 'off')
+        extraspaceremover = request.POST.get('extraspaceremover', 'off')
+        numberremover = request.POST.get('numberremover','off')
 
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        djtext = analyzed
+        #Check which checkbox is on
+        if removepunc == "on":
+            punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+            analyzed = ""
+            for char in djtext:
+                if char not in punctuations:
+                    analyzed = analyzed + char
 
-    if(fullcaps=="on"):
-        analyzed = ""
-        for char in djtext:
-            analyzed = analyzed + char.upper()
+            params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+            djtext = analyzed
 
-        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
-        djtext = analyzed
+        if(fullcaps=="on"):
+            analyzed = ""
+            for char in djtext:
+                analyzed = analyzed + char.upper()
 
-    if(extraspaceremover=="on"):
-        analyzed = ""
-        for index, char in enumerate(djtext):
-            # It is for if a extraspace is in the last of the string
-            if char == djtext[-1]:
-                    if not(djtext[index] == " "):
-                        analyzed = analyzed + char
+            params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+            djtext = analyzed
 
-            elif not(djtext[index] == " " and djtext[index+1]==" "):                        
-                analyzed = analyzed + char
+        if(extraspaceremover=="on"):
+            analyzed = ""
+            for index, char in enumerate(djtext):
+                # It is for if a extraspace is in the last of the string
+                if char == djtext[-1]:
+                        if not(djtext[index] == " "):
+                            analyzed = analyzed + char
 
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        djtext = analyzed
+                elif not(djtext[index] == " " and djtext[index+1]==" "):                        
+                    analyzed = analyzed + char
 
-    if (newlineremover == "on"):
-        analyzed = ""
-        for char in djtext:
-            if char != "\n" and char!="\r":
-                analyzed = analyzed + char
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+            djtext = analyzed
 
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-    
-    if (numberremover == "on"):
-        analyzed = ""
-        numbers = '0123456789'
+        if (newlineremover == "on"):
+            analyzed = ""
+            for char in djtext:
+                if char != "\n" and char!="\r":
+                    analyzed = analyzed + char
 
-        for char in djtext:
-            if char not in numbers:
-                analyzed = analyzed + char
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+            djtext = analyzed
         
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        djtext = analyzed
+        if (numberremover == "on"):
+            analyzed = ""
+            numbers = '0123456789'
 
-    
-    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on"):
-        return render(request, 'analyze_if_not_selected.html')
+            for char in djtext:
+                if char not in numbers:
+                    analyzed = analyzed + char
+            
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+            djtext = analyzed
+
+        
+        if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on"):
+            return render(request, 'analyze_if_not_selected.html')
         
 
-    return render(request, 'analyze.html', params)
+        return render(request, 'analyze.html', params)
 
 def about(request):
     return render(request, 'about.html')
