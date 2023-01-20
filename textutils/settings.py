64a94a392d46a489dd9bd9ac1e597f87c6d0a4b3
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-01-20 19:19:12 +0530

    Update bootstrap styles

--- textutils/settings.py
+++ textutils/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # django_heroku.settings(locals())
