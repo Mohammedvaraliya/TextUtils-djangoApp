096ab188a76ad93c326081d419afc0ffad295584
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-01-22 11:24:28 +0530

    update deployment files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 echo " BUILD START"
-python3 pip install -r requirements.txt
-python3 manage.py collectstatic
+python pip install django
+python manage.py collectstatic
 echo " BUILD END"
